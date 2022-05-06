49b069a5cc3a5830d9be3dc6ebdce1e58c157714
smalraju <100932589+smalraju@users.noreply.github.com>
2022-05-06 09:14:15 +0530

    Spring module

---
+++ hibernate-tutorial1/src/main/java/jdbc/TestJdbc.java
@@ -0,0 +1,22 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+    public static void main(String[] args){
+
+        String jdbcURL = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimeZone=UTC";
+        //String jdbcURL = "jdbc:mysql://127.0.0.1:3306/hb_student_tracker?useSSL=false&serverTimeZone=UTC";
+        String user="hbstudent";
+        String pass="Hbstudent@123";
+        try{
+            System.out.println("connecting to : "+ jdbcURL);
+            Connection connection = DriverManager.getConnection(jdbcURL,user,pass);
+            System.out.printf("Connection successful");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
